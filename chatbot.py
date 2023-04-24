81fb60a8070633c946d8a9bc7536cbb53af02f7a
LihuelMujica <lihuelmujica@gmail.com>
2023-04-24 07:13:34 -0300

    add: add memory to the chatbot

--- chatbot.py
+++ chatbot.py
@@ -10,16 +10,17 @@ username = input("Chatbot: Hola, soy chat gpt-3, ¿Cuál es tu nombre?:")
 
 print("Chatbot: Hola, " + username + "! ¿En qué puedo ayudarte hoy?")
 
+messages = []
 while True:
     try:
         user_input = input(username + ": ")
+        messages.append({ "role": "user", "content": user_input })
         res = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages = [
-                { "role": "user", "content": user_input }
-            ]
+            messages = messages
         )
         print("Chatbot: " + res["choices"][0]["message"]["content"])
     except KeyboardInterrupt:
+        print("")
         print("Chatbot: Adios!!")
         break
