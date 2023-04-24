0097274b32c51f24b2233c83f24faf9ccf7fa360
LihuelMujica <lihuelmujica@gmail.com>
2023-04-24 06:54:33 -0300

    add: openai api_key configuration

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,9 @@
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
+
+
 print("Bienvenido al chatbot en consola de Python")
 username = input("¿Cuál es tu nombre?:")
-print("Hola", username, "¿Cómo estás?")
