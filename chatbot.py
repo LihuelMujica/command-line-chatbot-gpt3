d55f3dbf3414744cf1b4f191e90ca9dd905e9877
LihuelMujica <lihuelmujica@gmail.com>
2023-04-24 08:44:30 -0300

    add: colours to the chat uwu

--- chatbot.py
+++ chatbot.py
@@ -5,6 +5,21 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
+def bold(text):
+    bold_start = "\033[1m"
+    bold_end = "\033[0m"
+    return bold_start + text + bold_end
+
+def blue(text):
+    blue_start = "\033[34m"
+    blue_end = "\033[0m"
+    return blue_start + text + blue_end
+
+def red(text):
+    red_start = "\033[31m"
+    red_end = "\033[0m"
+    return red_start + text + red_end
+
 def main():
 
     parser = argparse.ArgumentParser(description="Chatbot simple en linea de comandos usando OpenAI GPT-3 API")
@@ -13,24 +28,24 @@ def main():
     args = parser.parse_args()
 
     print("Bienvenido al chatbot en consola de Python")
-    username = input("Chatbot: Hola, soy chat gpt-3, ¿Cuál es tu nombre?:")
+    username = input(f"{red('Chatbot: ')}Hola, soy chat gpt-3, ¿Cuál es tu nombre?:")
 
-    print("Chatbot: Hola, " + username + "! ¿En qué puedo ayudarte hoy?")
+    print(f"{red('Chatbot: ')}Hola, " + username + "! ¿En qué puedo ayudarte hoy?")
 
     messages = [ { "role": "system", "content": f"Sos un chat de conversaciones y tu personalidad es {args.personality}" }]
     while True:
         try:
-            user_input = input(username + ": ")
+            user_input = input(blue(username + ": "))
             messages.append({ "role": "user", "content": user_input })
             res = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages = messages
             )
             messages.append(res["choices"][0]["message"].to_dict())
-            print("Chatbot: " + res["choices"][0]["message"]["content"])
+            print(red("Chatbot: ") + res["choices"][0]["message"]["content"])
         except KeyboardInterrupt:
             print("")
-            print("Chatbot: Adios!!")
+            print(f"{red('Chatbot: ')}Adios!!")
             break
 
 if __name__ == "__main__":
