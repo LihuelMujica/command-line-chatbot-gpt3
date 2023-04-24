d44ba2dcd85755c8c107daf0dfdf8557a06dc20b
LihuelMujica <lihuelmujica@gmail.com>
2023-04-24 07:07:07 -0300

    add: basic chatbot implementation without memory

--- chatbot.py
+++ chatbot.py
@@ -6,4 +6,20 @@ openai.api_key = config["OPENAI_API_KEY"]
 
 
 print("Bienvenido al chatbot en consola de Python")
-username = input("¿Cuál es tu nombre?:")
+username = input("Chatbot: Hola, soy chat gpt-3, ¿Cuál es tu nombre?:")
+
+print("Chatbot: Hola, " + username + "! ¿En qué puedo ayudarte hoy?")
+
+while True:
+    try:
+        user_input = input(username + ": ")
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages = [
+                { "role": "user", "content": user_input }
+            ]
+        )
+        print("Chatbot: " + res["choices"][0]["message"]["content"])
+    except KeyboardInterrupt:
+        print("Chatbot: Adios!!")
+        break
